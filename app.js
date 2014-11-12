3642eb9ee6273750b05f17fb4993b311aab3c520
John Wilson <johnleew@dirk.cs.washington.edu>
2014-11-12 14:17:51 -0800

    broke eveything, going to class now

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var myApp = angular.module('login',[]);
   
-myApp.controller('MainController', ['$scope', '$http', '$interval', function($scope, $http) {
-    
-    var moveDelta = 10;
-    var wizardWidth = 30;
-    var wizardHeight = 40;
+myApp.controller('MainController', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+
+    var wizardWidth = 30; // image wizard
+    var wizardHeight = 40; // image height
     var arenaWidth = 800;
     var arenaHeight = 800;
     var upArrow = 38;
@@ -12,39 +11,30 @@ myApp.controller('MainController', ['$scope', '$http', '$interval', function($sc
     var rightArrow = 39;
     var leftArrow = 37;
     
-    $scope.myWizard = $( '#myWizard' );
-    $("#arena").css("width", arenaWidth);
-    $("#arena").css("height", arenaHeight);
-    $("#myWizard").css("height", wizardHeight);
-    $("#myWizard").css("width", wizardWidth);
+    // this array stores the model view of each object in the arena
+    var objects = [];
     
-    $scope.joined = false;
-
-    $scope.joinGame = function() {
-        $scope.joined = true;
-        console.log($scope.joined);
-    };
+    var arena = $("#arena");
     
-    var changeVerticalPos = function(wizard, delta) {
-        var pos = parseInt(wizard.css("margin-top"));
-        var newPos = pos + delta;
-        if (newPos > 0 && newPos + wizardHeight < arenaHeight) {
-            wizard.css("margin-top", newPos);
-        }
-    };
+    var myWizard = {};
+    myWizard.hp = 100;
+    myWizard.xPos = 380;
+    myWizard.yPos = 380;
+    myWizard.xVelocity = 0;
+    myWizard.yVelocity = 0;
     
-    var changeHorizontalPos = function(wizard, delta) {
-        var pos = parseInt(wizard.css("margin-left"));
-        var newPos = pos + delta;
-        if (newPos > 0 && newPos + wizardWidth < arenaWidth) {
-            wizard.css("margin-left", newPos);
-        }
-    };
+    arena.css("width", arenaWidth);
+    arena.css("height", arenaHeight);
+    
+    var joined = false;
     
     $scope.keyDown = function($event) {
-        if (!$scope.joined)
+        console.log("key down");
+        console.log($event);
+        /*if (!$scope.joined)
             return;
-        console.log($event.keyCode);
+
+        //console.log($event.keyCode);
         switch($event.keyCode) {
             case (upArrow):
                 changeVerticalPos($scope.myWizard, 0 - moveDelta);
@@ -58,6 +48,42 @@ myApp.controller('MainController', ['$scope', '$http', '$interval', function($sc
             case (rightArrow):
                 changeHorizontalPos($scope.myWizard, moveDelta);
                 break;
-        }
+        } */
     };
+    
+    $scope.clickOnArena = function($event) {
+        console.log("offsetX: " + $event.offsetX);
+        console.log("offsetY: " + $event.offsetY);
+    };
+    
+    $scope.keyPress = function($event) {
+        console.log("key press");
+        console.log($event);
+    };
+    
+    $scope.keyUp = function($event) {
+        console.log("key up");
+        console.log($event);
+    };
+    
+    function drawModels(model) {
+        
+    };
+    
+    $scope.drawArena = function() {
+        
+    };
+
+    $scope.joinGame = function() {
+        $scope.joined = true;
+    };
+    
+    $interval(function() {
+        drawArena();
+    }, 100);
+    
 }]);
+
+
+
+
